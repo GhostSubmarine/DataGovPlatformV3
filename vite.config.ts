408a474e9b029854cf0cc38213e6009eb450e632
GhostSubmarine <1073944897@qq.com>
2024-12-10 09:59:15 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,34 @@ import vue from '@vitejs/plugin-vue'
 
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
-
+import pkg from './package.json' assert { type: 'json' };
+const timestamp = new Date().getTime();
 // 参考：https://cn.vitejs.dev/config/
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: Object.keys(pkg.dependencies),
+				},
+				chunkFileNames: ({ name }) => {
+					if (name === 'vendor') {
+						return `assets/js/[name]-[hash].js`; // 第三方库不添加时间戳
+					} else {
+						return `assets/js/[name]-[hash]-${timestamp}.js`; // 自定义文件名，使用时间戳保证唯一性
+					}
+				},
+				entryFileNames: ({ name }) => {
+					if (name === 'vendor') {
+						return `assets/js/[name]-[hash].js`; // 第三方库不添加时间戳
+					} else {
+						return `assets/js/[name]-[hash]-${timestamp}.js`; // 自定义文件名，使用时间戳保证唯一性
+					}
+				},
+				assetFileNames: `assets/[ext]/[name]-[hash].[ext]` // 资源文件添加时间戳
+			},
+		}
+	},
 	base: './',
 	resolve: {
 		// 配置别名
@@ -28,13 +53,13 @@ export default defineConfig({
 		port: 3000, // 端口号
 		open: false // 是否自动打开浏览器
 	},
-	 optimizeDeps: {
-	    include: [
-	      `monaco-editor/esm/vs/language/json/json.worker`,
-	      `monaco-editor/esm/vs/language/css/css.worker`,
-	      `monaco-editor/esm/vs/language/html/html.worker`,
-	      `monaco-editor/esm/vs/language/typescript/ts.worker`,
-	      `monaco-editor/esm/vs/editor/editor.worker`
-	    ], 
-	  }
+	optimizeDeps: {
+		include: [
+			`monaco-editor/esm/vs/language/json/json.worker`,
+			`monaco-editor/esm/vs/language/css/css.worker`,
+			`monaco-editor/esm/vs/language/html/html.worker`,
+			`monaco-editor/esm/vs/language/typescript/ts.worker`,
+			`monaco-editor/esm/vs/editor/editor.worker`
+		],
+	}
 })
